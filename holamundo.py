e5f8a128a5171c5cc9391fffbbad1d40b6e33619
Jose Rios <lithium@hotmail.es>
2020-09-25 00:18:05 -0300

    for simple

--- holamundo.py
+++ holamundo.py
@@ -1,10 +1,2 @@
-cantidad=0
-x=1
-n=int(input("Cuantas piezas cargara:"))
-while x<=n:
-    largo=float(input("Ingrese la medida de la pieza:"))
-    if largo>=1.2 and largo<=1.3:
-        cantidad=cantidad+1
-    x=x+1
-print("La cantidad de piezas aptas son")
-print(cantidad)
+for x in range(101):
+    print(x)
