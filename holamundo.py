6a7245905d8483675d001ceb60026db950016160
Jose Rios <lithium@hotmail.es>
2020-09-25 12:08:42 -0300

    Lista de enteros

--- holamundo.py
+++ holamundo.py
@@ -1,2 +1,10 @@
-for x in range(100,115,5):
-    print(x)
+lista=[10,7,3,7,2]
+suma=0
+x=0
+while x<len(lista):
+    suma=suma+lista[x]
+    x=x+1
+print("Los elementos de la lista son")
+print(lista)
+print("La suma de todos sus elementos es")    
+print(suma)
