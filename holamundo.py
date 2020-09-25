04002e2916b658de2e6eecaaf44b033316f1bdaa
Jose Rios <lithium@hotmail.es>
2020-09-25 17:33:05 -0300

    Lectura del request de una pagina web con urllib

--- holamundo.py
+++ holamundo.py
@@ -1,12 +1,6 @@
-def operar(v1,v2,fn):
-    return fn(v1,v2)
+from urllib import request
 
-resu1=operar(10,3,lambda x1,x2: x1+x2)
-print(resu1)
-
-resu2=operar(10,3,lambda x1,x2: x1-x2)
-print(resu2)
-
-print(operar(30,10,lambda x1,x2: x1*x2))
-
-print(operar(10,2,lambda x1,x2: x1/x2))
+pagina=request.urlopen("http://www.scratchya.com.ar/pythonya/ejercicio336/pagina1.html")
+datos=pagina.read()
+datosutf8=datos.decode("utf-8")
+print(datosutf8)
