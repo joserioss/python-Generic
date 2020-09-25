1eded769cd21773231f1bf6f8ed50d5aab4b6a35
Jose Rios <lithium@hotmail.es>
2020-09-25 13:24:00 -0300

    POO: Alumno aprobado

--- holamundo.py
+++ holamundo.py
@@ -1,18 +1,28 @@
-class Persona:
+class Alumno:
 
-    def inicializar(self,nom):
-        self.nombre=nom
+    def inicializar(self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
 
     def imprimir(self):
-        print("Nombre",self.nombre)
+        print("Nombre:",self.nombre)
+        print("Nota:",self.nota)
+
+    def mostrar_estado(self):
+        if self.nota>=4:
+            print("Aprobado")
+        else:
+            print("Reprobado")
 
 
 # bloque principal
 
-persona1=Persona()
-persona1.inicializar("Pedro")
-persona1.imprimir()
+alumno1=Alumno()
+alumno1.inicializar("diego",2)
+alumno1.imprimir()
+alumno1.mostrar_estado()
 
-persona2=Persona()
-persona2.inicializar("Carla")
-persona2.imprimir()
+alumno2=Alumno()
+alumno2.inicializar("ana",10)
+alumno2.imprimir()
+alumno2.mostrar_estado()
