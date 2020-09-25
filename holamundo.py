516eb4dd263fef17df21c5e9a9dff81fcee7291e
Jose Rios <lithium@hotmail.es>
2020-09-25 13:11:52 -0300

    For sin subindices

--- holamundo.py
+++ holamundo.py
@@ -1,14 +1,4 @@
-def titulo_subrayado(titulo,caracter="*"):
-    print(titulo)
-    print(caracter*len(titulo))
+lista=[2, 3, 50, 7, 9]
 
-def calcular_sueldo(nombre,costohora,cantidadhoras):
-    sueldo=costohora*cantidadhoras
-    print(nombre,"trabajo",cantidadhoras,"y cobra un sueldo de",sueldo)
-
-
-# bloque principal
-
-calcular_sueldo("juan",10,120)
-calcular_sueldo(costohora=12,cantidadhoras=40,nombre="ana")
-calcular_sueldo(cantidadhoras=90,nombre="luis",costohora=7)
+for elemento in lista:
+    print(elemento)
