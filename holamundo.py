63e46136195ea7c9cbf5d0fd2735e70dae1f13ae
Jose Rios <lithium@hotmail.es>
2020-09-25 12:23:46 -0300

    funciones con parametros opcionales

--- holamundo.py
+++ holamundo.py
@@ -1,10 +1,9 @@
-lista=[10,7,3,7,2]
-suma=0
-x=0
-while x<len(lista):
-    suma=suma+lista[x]
-    x=x+1
-print("Los elementos de la lista son")
-print(lista)
-print("La suma de todos sus elementos es")    
-print(suma)
+def titulo_subrayado(titulo,caracter="*"):
+    print(titulo)
+    print(caracter*len(titulo))
+
+
+# bloque principal
+
+titulo_subrayado("Sistema de Administracion")
+titulo_subrayado("Ventas","´")
