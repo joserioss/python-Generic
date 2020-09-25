5783395dfc01c2750cfea278ee531a35b0399bc6
Jose Rios <lithium@hotmail.es>
2020-09-25 17:34:46 -0300

    Lectura de pagina web y grabacion en archivo local

--- holamundo.py
+++ holamundo.py
@@ -2,5 +2,12 @@ from urllib import request
 
 pagina=request.urlopen("http://www.scratchya.com.ar/pythonya/ejercicio336/pagina1.html")
 datos=pagina.read()
-datosutf8=datos.decode("utf-8")
-print(datosutf8)
+archivo1=open("pagina1.html","wb")
+archivo1.write(datos)
+archivo1.close()
+
+imagen=request.urlopen("http://www.scratchya.com.ar/pythonya/ejercicio336/imagen1.jpg")
+datos=imagen.read()
+archivo2=open("imagen1.jpg","wb")
+archivo2.write(datos)
+archivo2.close()
