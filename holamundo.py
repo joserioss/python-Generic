305ce3412d4a47366981d9c34892887618088a48
Jose Rios <lithium@hotmail.es>
2020-09-25 14:05:07 -0300

    funcion de orden superior

--- holamundo.py
+++ holamundo.py
@@ -1,6 +1,24 @@
-archi1=open("datos.txt","r+") 
-contenido=archi1.read()
-print(contenido)
-archi1.write("Otra línea 1\n")
-archi1.write("Otra línea 2\n")
-archi1.close()
+def operar(v1,v2,fn):
+    return fn(v1,v2)
+
+def sumar(x1,x2):
+    return x1+x2
+
+def restar(x1,x2):
+    return x1-x2
+
+def multiplicar(x1,x2):
+    return x1*x2
+
+def dividir(x1,x2):
+    return x1/x2
+
+resu1=operar(10,3,sumar)
+print(resu1)
+
+resu2=operar(10,3,restar)
+print(resu2)
+
+print(operar(30,10,multiplicar))
+
+print(operar(10,2,dividir))
